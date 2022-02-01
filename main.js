39f77046b7382d5898b21990d795a59870cb4b22
sakethuse <86656550+sakethuse@users.noreply.github.com>
2022-02-01 17:51:44 +0530

    NOice

---
+++ main.js
@@ -0,0 +1,121 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+my_keydown;
+
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e){
+	keyPressed = e.keyCode;
+	console.log(keyPressed)
+	
+	if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90));
+	{
+		aplhabetkey();
+		console.log("AlpabetKey");
+	}
+		
+	if (keyPressed >=48 && keyPressed<=57)
+	{
+		numberkey();
+		console.log("Numberky");
+	}
+		
+		
+	if(keyPressed == 17)
+	{
+		specialkey();
+	    console.log("spacial");
+	}
+	
+	if(keyPressed == 18)
+    {
+		specialkey();
+		console.log("special");
+	}
+	if(keyPressed == 27)
+	{
+	    specialkey();
+		console.log("spacialy");
+	}
+
+    
+		
+	else if (condition) {
+		
+	} else {
+		otherkey();
+	    document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+	{
+	   
+	}
+}
+	
+
+
+
+
+
+
+function aplhabetkey(){
+	 img_image="Alpkey.png";
+	add();
+
+}
+function numberkey(){
+	 img_image="numkey.png";
+	add();
+}
+function arrowkey(){
+	 img_image="Arrkey.png";
+	add();
+}
+function specialkey(){
+	 img_image="spkey.png";
+	add();
+}
+function otherkey(){
+	 img_image="otherkey.png";
+	add();
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
